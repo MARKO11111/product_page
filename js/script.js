394854b919126cd8ab6d39f2440a00d8564b6e73
Marko Bilic <bilmarko@gmail.com>
2023-04-24 15:01:10 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+//modal 
+
+
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("contact_btn");
+
+var span = document.getElementsByClassName("close_icon")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
